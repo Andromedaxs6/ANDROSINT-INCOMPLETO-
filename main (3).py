54461c71e8efd7f9183d034722d4f5da96df4419
Andromeda404 <121472410+Andromedaxs6@users.noreply.github.com>
2023-04-23 15:21:41 -0300

    Add files via upload

---
+++ main (3).py
@@ -0,0 +1,6 @@
+import os
+os.system("sudo apt install figlet && clear && figlet ANDROSINT")
+print()
+print("ANDROSINT BY ANDROMEDA | 7th Hell Team")
+alvo = int(input("NOME DO ALVO:"))
+print("www.google.com" + alvo)
