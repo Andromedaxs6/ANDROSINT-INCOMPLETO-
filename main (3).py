3638d1e9fb53fd44972bc56832f9080685867000
Andromeda404 <121472410+Andromedaxs6@users.noreply.github.com>
2023-04-23 15:34:28 -0300

    Update main (3).py

--- main (3).py
+++ main (3).py
@@ -1,6 +1,19 @@
 import os
+import time
 os.system("sudo apt install figlet && clear && figlet ANDROSINT")
 print()
 print("ANDROSINT BY ANDROMEDA | 7th Hell Team")
-alvo = int(input("NOME DO ALVO:"))
-print("www.google.com" + alvo)
+alvo = input("NOME DO ALVO:")
+print("www.google.com/"+ alvo)
+print("")
+print("")
+voltar = int(input("Para Voltar Ao Menu, Digite 0: "))
+if voltar == 0:
+    os.system("python3 main\ \(3\).py")
+else:
+    print("Comando Desconhecido, Tentando Entender Comando...")
+    time.sleep(1)
+    ("Comando Desconhecido...")
+    time.sleep(1)
+    ("Reiniciando sistema""")
+    os.system("python3 main\ \(3\).py")
